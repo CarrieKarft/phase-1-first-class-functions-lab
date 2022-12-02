914d0f0edf3edd49d1fe118dd74213ac4c40936a
Carrie Kraft <carriekarft@gmail.com>
2022-12-01 22:04:55 -0800

    commit tree ae5e109a8933304d6fa4ed9012758258f275d360

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Code your solution in this file!
+// functions for minipulating the array of drivers
+// const driverArray = ['Antonia', 'Nuru', 'Amari', 'Mo']
+const returnFirstTwoDrivers = function(array) {
+    return array.slice(0, 2)
+}
+
+
+const returnLastTwoDrivers = function(array) {
+    return array.slice(2, 4);
+}
+
+
+
+// array of above functions
+let selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+// functions for multiplying fare by a given interger
+// this is definitly incorrect
+const createFareMultiplier = function(multiplyValue) {
+    return function (fare) {
+        return fare * multiplyValue
+  };
+};
+
+const fareDoubler = createFareMultiplier(2);
+
+const fareTripler = createFareMultiplier(3);
+
+function selectDifferentDrivers(arrayOfDNames, driversFunction) {
+    return driversFunction(arrayOfDNames);
+}
+console.log(selectDifferentDrivers(['Antonia', 'Nuru', 'Amari', 'Mo'], returnFirstTwoDrivers));
+
+
